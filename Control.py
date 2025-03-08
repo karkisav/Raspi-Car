010893a322e02425006d900c43a67b150d063494
karkisav <saurav.karki2@icloud.com>
2025-03-09 02:36:19 +0530

    Attempting to Turn Left n RIGHt

--- Control.py
+++ Control.py
@@ -35,7 +35,7 @@ pwm_b.start(0)
 
 class PiCar:
     def __init__(self):
-        self.speed = 3
+        self.speed = 5
 
     def forward(self, speed = None):
         if speed is not None:
@@ -64,12 +64,12 @@ class PiCar:
         pwm_b.ChangeDutyCycle(self.speed)
 
 
-    def turn_left(self, turn_speed = 1):
+    def turn_left(self, turn_speed = 3):
         # Slow down left side, maintain right side
         pwm_a.ChangeDutyCycle(self.speed - turn_speed)
         pwm_b.ChangeDutyCycle(self.speed)
 
-    def turn_right(self, turn_speed = 1):
+    def turn_right(self, turn_speed = 3):
         # Maintian down left side, slow down right side
         pwm_a.ChangeDutyCycle(self.speed)
         pwm_b.ChangeDutyCycle(self.speed - turn_speed)
@@ -116,10 +116,10 @@ if __name__ == "__main__":
         time.sleep(1)
         print("Done !!!")
 
-        print("Changing lane To left lane")
-        car.change_lane_left()
-        time.sleep(1)
-        print("Done !!!")
+        # print("Changing lane To left lane")
+        # car.change_lane_left()
+        # time.sleep(1)
+        # print("Done !!!")
 
         print("All tests done now !!")
         print("stopping the car now!")
